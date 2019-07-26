909693bd21b2e6a39d86f1e4e2e2077550573d94
Dmytro9 <dpoliuliuk@gmail.com>
2019-07-26 19:31:00 +0300

    hide keys

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
 
 passport.use(
 	new GoogleStrategy({
-		clientID:
-			'863543759058-msai6sa06ng1ajvvlr334ttih29edebt.apps.googleusercontent.com',
-		clientSecter: 'P7NyjOzpA5FcLlw8KuKrWVYT'
+		clientID: keys.google.clientID,
+		clientSecter: keys.google.clientSecret
 	}),
 	() => {}
 );
